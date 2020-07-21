e7681c8f95694e22311b1e51835f2627e67e36da
Mehmet Helvacıköylü <mhelvacikoylu@gmail.com>
2020-07-22 00:54:08 +0300

    sample project

--- src/Samples/Sample.Ioc/Program.cs
+++ src/Samples/Sample.Ioc/Program.cs
@@ -8,11 +8,20 @@ namespace Sample.Ioc
     public interface ITest
     {
         int Value { get; set; }
+        ITest2 T2 { get; set; }
+        ITest3 T3 { get; }
     }
 
     public class Test : ITest
     {
         public int Value { get; set; }
+        public ITest2 T2 { get; set; }
+        public ITest3 T3 { get; }
+
+        public Test(ITest3 test3)
+        {
+            T3 = test3;
+        }
     }
 
     public interface ITest2
@@ -23,22 +32,32 @@ namespace Sample.Ioc
     {
     }
 
+    public interface ITest3
+    {
+    }
+
+    public class Test3 : ITest3
+    {
+    }
+
     class Program
     {
         static async Task Main(string[] args)
         {
-            IServiceContainer container = new ServiceContainer();
-            container.AddSingleton<ITest>(new Test {Value = 123});
+            IServiceCollection container = new ServiceContainer();
             container.AddTransient<ITest2, Test2>();
+            container.AddTransient<ITest3, Test3>();
+            container.AddSingleton<ITest, Test>();
 
             //optional, for checking missing references and circular references.
             //if there is a missing ref or circularity, throws exception
-            container.CheckServices();
+            //container.CheckServices();
 
-            var t1 = await container.Get<ITest>();
-            var t2 = container.GetService<ITest>();
-            Console.WriteLine(t1.Value);
-            Console.WriteLine(t2.Value);
+            //var t1 = await container.Get<ITest>();
+            //var t2 = container.GetService<ITest>();
+            //Console.WriteLine(t1.Value);
+            //Console.WriteLine(t2.Value);
+
 
             var provider = container.BuildServiceProvider();
 
